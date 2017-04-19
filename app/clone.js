0fce4b3b8b508244f0c13b63e59970397bd5cc26
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-19 11:12:59 +0200

    deep clone

--- app/clone.js
+++ app/clone.js
@@ -4,19 +4,26 @@
 	var clone = function(o) {
 		var result = {};
 		for (var p in o) {
-			result[p] = o[p];
+			if (o[p] !== null && typeof o[p] === 'object') {
+				result[p] = clone(o[p]);
+			} else {
+				result[p] = o[p];
+			}
+
 		}
 		return result;
 	};
 
 	var a = {
-		x: 45,
+		x: {
+			z: 45
+		},
 		y: 52
 	};
-	console.log('a.x', a.x);
+	console.log('a.x.z', a.x.z);
 	var b = clone(a);
-	console.log('b.x', b.x);
-	a.x = 31;
-	console.log('a.x', a.x);
-	console.log('b.x', b.x);
+	console.log('b.x.z', b.x.z);
+	a.x.z = 31;
+	console.log('a.x.z', a.x.z);
+	console.log('b.x.z', b.x.z);
 })();
