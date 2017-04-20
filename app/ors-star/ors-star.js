a067a51f72618300414d5812b7572d423afb8beb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-20 10:12:26 +0200

    add: directive ors-star

---
+++ app/ors-star/ors-star.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('ors-star', []);
+
+	app.directive('orsStar', function() {
+		return {
+			restrict: 'E',
+			template: '<img src="./ors-star/img/yellow_star.png"/>'
+		};
+	});
+})();
