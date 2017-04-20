6fdf2f6b998ee471b0692f583573bb5289dbc63f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-20 14:13:44 +0200

    exo promise: suite et fin

--- app/ors-route/ors-route.js
+++ app/ors-route/ors-route.js
@@ -40,19 +40,32 @@
 		$stateProvider.state(sevicesState);
 		$stateProvider.state(clientsState);
 		$stateProvider.state(contactState);
-		
+
 		$urlRouterProvider.otherwise('/');
 		$locationProvider.html5Mode(true);
 	});
 
-	app.controller('ServicesCtrl', function ServicesCtrl($http) {
+	app.controller('ServicesCtrl', function ServicesCtrl($http, $q) {
 		'ngInject';
 		console.log('ServicesCtrl', arguments);
 		var ctrl = this;
 		ctrl.start = function() {
 			console.log('start', arguments);
 			$http.get('../ws/s1').then(function(response) {
-				console.log('response', response);
+				console.log('response 1', response);
+				return $q.all([
+					$http.get('../ws/s2'),
+					$http.get('../ws/s3'),
+					$http.get('../ws/s4').then(function(response) {
+						console.log('response 4', response);
+						return $http.get('../ws/s5');
+					})
+				]);
+			}).then(function(responses) {
+				console.log('responses 2, 3, (4, 5)', responses);
+				return $http.get('../ws/s6');
+			}).then(function(response) {
+				console.log('response 6', response);
 			}).catch(function(error) {
 				console.error('error', error);
 			});
