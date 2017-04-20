b07d39daaa8f02a316f6d619240481812d484464
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-20 10:55:14 +0200

    add: ajout des étoiles dans le site web

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,8 +6,24 @@
 	app.directive('orsStar', function() {
 		return {
 			restrict: 'E',
-			template: function() {
-				return '<img src="./ors-star/img/yellow_star.png"/>';
+			scope: {
+				note: '='
+			},
+			controller: function($scope, $element, $attrs) {
+				'ngInject';
+				console.log('orsStar template', arguments);
+				$scope.$watch('note', function() {
+					console.log('watch', arguments);
+					var note = $scope.note || 3;
+					var html = '';
+					for (var i = 0; i < note; i++) {
+						html += '<img src="./ors-star/img/yellow_star.png"/>';
+					}
+					for (var i = note; i < 5; i++) {
+						html += '<img src="./ors-star/img/white_star.png"/>';
+					}
+					$element.html(html);
+				});
 			}
 		};
 	});
