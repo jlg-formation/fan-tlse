7cfe6ffeda2b526448c5b9361a7cf414d2b4e147
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-04-20 16:45:52 +0200

    add : packaging

--- app/main.js
+++ app/main.js
@@ -1,26 +1,38 @@
-(function() {
-	'use strict';
+'use strict';
+var jQuery = require('jquery');
+window.$ = jQuery;
+window.jQuery = jQuery;
 
-	var app = angular.module('main', ['ors-route', 'ors-star', 'angularSpinner']);
+require('./layout/scripts/jquery.backtotop.js');
+require('./layout/scripts/jquery.mobilemenu.js');
+require('./layout/scripts/jquery.placeholder.min.js');
 
-	app.directive('orsHeader', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-header.html',
-		};
-	});
+require('angular');
+require('angular-ui-router');
+require('./ors-route/ors-route.js');
+require('./ors-star/ors-star.js');
+require('spin.js/spin.js');
+require('angular-spinner/dist/angular-spinner.js');
 
-	app.directive('orsBody', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-body.html',
-		};
-	});
+var app = angular.module('main', ['ors-route', 'ors-star', 'angularSpinner']);
 
-	app.directive('orsFooter', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'tmpl/ors-footer.html',
-		};
-	});
-})();
+app.directive('orsHeader', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'tmpl/ors-header.html',
+	};
+});
+
+app.directive('orsBody', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'tmpl/ors-body.html',
+	};
+});
+
+app.directive('orsFooter', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'tmpl/ors-footer.html',
+	};
+});
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+// const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: {
+		bundle: './app/main.js'
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, './app/wpk')
+	},
+	module: {
+		rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'ng-annotate-loader'
+				},{
+                    loader: 'babel-loader'
+                }]
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader?minimize&sourceMap'
+				})
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader?minimize&sourceMap!sass-loader?sourceMap'
+				})
+			},
+			{
+				test: /\.html$/,
+				use: [{
+					loader: 'ngtemplate-loader',
+					options: {
+						relativeTo: 'app'
+					}
+				}, {
+					loader: 'html-loader',
+					options: {
+						attrs: 'img-svg:src',
+						root: path.resolve('./app')
+					}
+				}]
+			}
+		]
+	},
+	devtool: 'source-map',
+	plugins: [
+		new ExtractTextPlugin('[name].css'),
+		// new webpack.optimize.UglifyJsPlugin({
+		// 	sourceMap: true,
+		// 	compress: {
+		// 		warnings: false
+		// 	},
+		// 	output: {
+		// 		comments: false,
+		// 	}
+		// })
+	]
+};
